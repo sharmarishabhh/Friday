64a1a8fd8dfb20277bb5687f9d00a44c378964f0
Rishabh Sharma <148844430+sharmarishabhh@users.noreply.github.com>
2024-12-06 23:07:33 -0800

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "devil eyes": "https://www.youtube.com/watch?v=Os9j2kafFfI",
+    "Dandelions": "https://www.youtube.com/watch?v=W8a4sUabCUo",
+    "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
+    "gangster": "https://www.youtube.com/watch?v=fPO76Jlnz6c",
+    "unstoppable": "https://www.youtube.com/watch?v=YaEG2aWJnZ8"
+}
